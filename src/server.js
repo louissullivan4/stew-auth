5eb7f83827176ec1040e584bb2fce698e4ce8843
Louis Sullivan <sullivanlouis0@gmail.com>
2024-01-02 16:13:57 +0000

    Update MongoDB connection in server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
