44bfb2b4e41b3824ced9b01d1cc617fc533336f2
Louis Sullivan <sullivanlouis0@gmail.com>
2024-01-02 15:39:22 +0000

    Add server.js file with MongoDB connection and authentication routes

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
