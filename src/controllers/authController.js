8c3e5e6481abdfa828069aed5270f0f3b3e290b1
Louis Sullivan <sullivanlouis0@gmail.com>
2024-01-02 15:39:26 +0000

    Add signUp and login functions to authController.js

---
+++ src/controllers/authController.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const signUp = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+        res.status(201).send('User created successfully');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (user && await bcrypt.compare(password, user.password)) {
+            const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.status(200).json({ token });
+        } else {
+            res.status(401).send('Invalid credentials');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+module.exports = { signUp, login };
